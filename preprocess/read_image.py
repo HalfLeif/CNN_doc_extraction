b5243cc5530be0a362ad4aef2dbed31ca2fd0ee5
Leif Schelin <scleif@student.chalmers.se>
2017-02-16 12:06:47 +0100

    Refactored debug code into functions

--- preprocess/read_image.py
+++ preprocess/read_image.py
@@ -2,22 +2,7 @@
 import grayscale as gray
 import tensorflow as tf
 
-
 import sys, os
-
-
-
-filename_queue = tf.train.string_input_producer(
-    tf.train.match_filenames_once("./data/*.jpg"))
-
-image_reader = tf.WholeFileReader()
-
-_, image_file = image_reader.read(filename_queue)
-
-# Grayscale image: channels=1
-# Can downscale with: ratio=2/4/8
-image = tf.image.decode_jpeg(image_file, channels=1, ratio=2)
-
 
 def printImage(image_tensor):
     rows = len(image_tensor)
@@ -36,22 +21,41 @@ def printImage(image_tensor):
         sys.stdout.write('\n')
 
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    print("Session started")
+def readJpg(dirname):
+    glob = os.path.join(dirname, '*.jpg')
+    filename_queue = tf.train.string_input_producer(
+        tf.train.match_filenames_once(glob))
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+    image_reader = tf.WholeFileReader()
+    _, image_file = image_reader.read(filename_queue)
 
-    print("Evaluate...")
+    # Grayscale image: channels=1
+    # Can downscale with: ratio=2/4/8
+    return tf.image.decode_jpeg(image_file, channels=1, ratio=2)
 
-    test, threshold = gray.otsusGlobalThreshold(image)
 
-    result = sess.run([test, threshold])
-    print("Evaluated")
-    print("Threshold: " + str(result[1]))
-    image_tensor = result[0]
-    printImage(image_tensor)
+def evaluateImage(image):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print("Session started")
 
-    coord.request_stop()
-    coord.join(threads)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        print("Evaluate...")
+
+        test, threshold = gray.otsusGlobalThreshold(image)
+        print("Gray...")
+
+        result = sess.run([test, threshold])
+        print("Evaluated")
+        print("Threshold: " + str(result[1]))
+        image_tensor = result[0]
+        printImage(image_tensor)
+
+        coord.request_stop()
+        coord.join(threads)
+
+if __name__ == '__main__':
+    image = readJpg(sys.argv[1])
+    evaluate(image)
