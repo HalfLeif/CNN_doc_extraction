db8e3e0afa0cf30aa95e5023f4abc7144c13cb68
Leif Schelin <scleif@student.chalmers.se>
2017-02-27 12:28:03 +0100

    Implemented soft attention model

--- network/conv.py
+++ network/conv.py
@@ -20,6 +20,40 @@ def deepEncoder(image):
         net = addLayer(net, 128, '4')
         return net
 
+def attend_vector(feature_vector, keep_prob):
+    ''' Returns a scalar in range [0-1] for this activation unit.'''
+    print('ATT_VECTOR', feature_vector.get_shape())
+    with slim.arg_scope([slim.fully_connected],
+                        activation_fn=tf.nn.relu,
+                        weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
+                        weights_regularizer=slim.l2_regularizer(0.0005)):
+
+        net = tf.expand_dims(feature_vector, 0)
+        net = slim.fully_connected(net, 1024, scope='attend_1')
+        net = slim.dropout(net, keep_prob, scope='att_dropout1')
+
+        net = slim.fully_connected(net, 128, scope='attend_2')
+        net = slim.dropout(net, keep_prob, scope='att_dropout2')
+
+        attention = slim.fully_connected(net, 1, activation_fn=tf.sigmoid, scope='attend_out')
+        return tf.squeeze(attention)
+
+def attend_image(activation_3d, keep_prob):
+    ''' Takes an activation map with units of depth D,
+        returns a weighted sum of the activation units for each batch item.
+        The weights in the sum is determined by a neural network.'''
+
+    print('ATT_IMAGE', activation_3d.get_shape())
+    atts = tf.map_fn(lambda vectors: tf.map_fn(lambda vec: attend_vector(vec, keep_prob), vectors), activation_3d)
+    print('ATTS', atts.get_shape())
+    weighted = tf.expand_dims(atts, -1) * activation_3d
+    vector = tf.reduce_sum(weighted, axis=[0, 1]) / tf.reduce_sum(atts)
+    return vector
+
+def attend(net, keep_prob):
+    ''' Performs soft attention for all activations in this batch.'''
+    return tf.map_fn(lambda img: attend_image(img, keep_prob), net)
+
 
 def switchFirstTwo(ds):
     '''Swaps the first two elements in a list.'''
@@ -41,7 +75,6 @@ def expandDigits(digit_stack):
     number_log = tf.reshape(sum_log, [-1])
     return number_log
 
-
 def decodeNumber(net, keep_prob):
     ''' Decodes activation map into a 3 digit number.
         Uses a separate output for whether the input contains any digits or not.
@@ -51,6 +84,7 @@ def decodeNumber(net, keep_prob):
                         weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
                         weights_regularizer=slim.l2_regularizer(0.0005)):
         net = slim.flatten(net)
+        print('FLATTEN: ', net.get_shape())
         net = slim.fully_connected(net, 1024, scope='fc1')
         net = slim.dropout(net, keep_prob, scope='dropout1')
 
