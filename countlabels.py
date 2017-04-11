01bf381bdcf3b379cf0703414edc79ae0618db2c
Leif Schelin <schelinleif@gmail.com>
2017-04-11 09:35:22 +0200

    Added code to verify assumption that all labels are consecutive

--- countlabels.py
+++ countlabels.py
@@ -3,7 +3,30 @@ import loading.load_swe as swe
 
 from collections import Counter
 
-if __name__ == '__main__':
-    _, years = swe.loadTrainingSet();
+def printYearCounts(years):
     for y, count in Counter(years).items():
         print(y, count)
+
+def isConsecutive(yearlist):
+    last = None
+    for year in yearlist:
+        if last and last != year - 1:
+            return False
+        last = year
+    return True
+
+def verifyConsecutive(years):
+    errors = 0
+    for yearlist in years:
+        if not isConsecutive(yearlist):
+            print(yearlist)
+            errors = errors + 1
+    print('There are', errors, 'inconsecutive year labels out of', len(years))
+
+if __name__ == '__main__':
+    _, years = swe.loadTrainingSet();
+    verifyConsecutive(years)
+    _, years = swe.loadTestSet();
+    verifyConsecutive(years)
+    _, years = swe.loadEvalSet();
+    verifyConsecutive(years)
