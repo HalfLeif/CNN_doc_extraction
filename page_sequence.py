d5bdc651e141851b335b310c7a4909448b48b087
Leif Schelin <schelinleif@gmail.com>
2017-04-12 09:40:21 +0200

    Can now see jump distribution for years in different collections

--- page_sequence.py
+++ page_sequence.py
@@ -1,26 +1,75 @@
 
+import ast
 import os
-import sys
 
-def analyzeCollection(filename):
-    print('Analyzing', filename)
+def buildImageDict(filename):
     page_dict = {}
     with open(filename, 'r', newline='\n') as csvfile:
         for line in csvfile:
             fields = line.split(' | ')
             image_id = fields[0]
             years = fields[1]
-            page_nbr = int(fields[2])
-            book_name = ' | '.join(fields[3:])
+            page_nbr = fields[2]
+            book = fields[3:]
 
-            page_dict[page_nbr] = (years, book_name, image_id)
+            page_dict[page_nbr] = (years, book, image_id)
+    return page_dict
 
-    for key in sorted(page_dict.keys()):
-        print(key, page_dict[key])
+def traverseSorted(dictionary):
+    for key in sorted(dictionary.keys()):
+        yield dictionary[key]
+
+def increment(distribution, key, incr):
+    if key in distribution:
+        distribution[key] += incr
+    else:
+        distribution[key] = incr
+
+def addExample(distribution, prev, current):
+    num_combinations = len(prev) * len(current)
+    for y1 in prev:
+        for y2 in current:
+            diff = y2 - y1
+            increment(distribution, diff, 1.0/num_combinations)
+
+def isSameBook(book1, book2):
+    if not book1 or not book2:
+        return False
+
+    # All books have 3 fields: Parish, series, volume
+    for field in range(3):
+        if book1[field] != book2[field]:
+            return False
+
+    return True
+
+def analyzeCollection(filename, distribution, denominator):
+    print('Analyzing', filename)
+    page_dict = buildImageDict(filename)
+
+    prev_year_list = []
+    prev_book = None
+    for year_str, book, image_id in traverseSorted(page_dict):
+        year_list = ast.literal_eval(year_str)
+
+        if isSameBook(prev_book, book):
+            addExample(distribution, prev_year_list, year_list)
+            denominator += 1
+        else:
+            prev_book = book
+
+        prev_year_list = year_list
+
+    return distribution, denominator
 
 if __name__ == '__main__':
     page_index_dir = os.path.join('data', 'labels_index', 'page_index')
+    distribution = {}
+    denominator = 0
+
     for filename in os.listdir(page_index_dir):
         path = os.path.join(page_index_dir, filename)
-        analyzeCollection(path)
-        break
+        distribution, denominator = analyzeCollection(path, distribution, denominator)
+
+    for key in sorted(distribution.keys()):
+        print(key, distribution[key])
